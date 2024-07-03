096e0fea0076e5f37ea95e2e7ee4dc0fe5519bfc
Better-Than-You <133567908+Better-Than-You@users.noreply.github.com>
2024-07-03 11:54:14 +0530

    Initial upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+        // const observer = new IntersectionObserver((entry) => {
+        //         console.log(entry.target);
+        //         entry.target.classList.toggle('show', entry.isIntersecting);
+        //         if (entry.isIntersecting) {
+        //             observer.unobserve(entry.target)
+        //         }
+        // }
+        // )
+
+        // const hiddenElements = document.querySelector('.hidden');
+        //     observer.observe(hiddenElements);
